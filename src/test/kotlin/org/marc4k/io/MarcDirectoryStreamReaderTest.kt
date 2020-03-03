462575bc003e44265ef22fd859863b08884c99a8
Philip Puffinburger <ppuffinburger@gmail.com>
2020-03-03 10:32:56 -0800

    Redid MarcDirectoryStreamReaderTest.kt tests because how a directory walk happens can change between OSs and even OS releases.

--- src/test/kotlin/org/marc4k/io/MarcDirectoryStreamReaderTest.kt
+++ src/test/kotlin/org/marc4k/io/MarcDirectoryStreamReaderTest.kt
@@ -12,44 +12,34 @@ import java.nio.file.Files
 
 internal class MarcDirectoryStreamReaderTest {
     @Test
-    @DisabledOnOs(OS.WINDOWS)
     fun `test MarcDirectoryStreamReader(File, MarcDataDecoder)`() {
+        val leaders = mutableSetOf("06506cz  a2201093n  4500", "06484cz   2201093n  4500")
         MarcDirectoryStreamReader(File("src/test/resources/records/walktest"), Marc21DataDecoder()).use { reader ->
             var count = 0
-            for ((index, record) in reader.withIndex()) {
-                when (index) {
-                    0 -> {
-                        assertThat(record.leader.getData()).isEqualTo("06506cz  a2201093n  4500")
-                        count++
-                    }
-                    1 -> {
-                        assertThat(record.leader.getData()).isEqualTo("06484cz   2201093n  4500")
-                        count++
-                    }
-                }
+            for (record in reader) {
+                val leader = record.leader.getData()
+                assertThat(leaders).contains(leader)
+                leaders.remove(leader)
+                count++
             }
             assertThat(count).isEqualTo(2)
+            assertThat(leaders).isEmpty()
         }
     }
 
     @Test
-    @DisabledOnOs(OS.WINDOWS)
     fun `test MarcDirectoryStreamReader(String, MarcDataDecoder)`() {
+        val leaders = mutableSetOf("06506cz  a2201093n  4500", "06484cz   2201093n  4500")
         MarcDirectoryStreamReader("src/test/resources/records/walktest", Marc21DataDecoder()).use { reader ->
             var count = 0
-            for ((index, record) in reader.withIndex()) {
-                when (index) {
-                    0 -> {
-                        assertThat(record.leader.getData()).isEqualTo("06506cz  a2201093n  4500")
-                        count++
-                    }
-                    1 -> {
-                        assertThat(record.leader.getData()).isEqualTo("06484cz   2201093n  4500")
-                        count++
-                    }
-                }
+            for (record in reader) {
+                val leader = record.leader.getData()
+                assertThat(leaders).contains(leader)
+                leaders.remove(leader)
+                count++
             }
             assertThat(count).isEqualTo(2)
+            assertThat(leaders).isEmpty()
         }
     }
 
